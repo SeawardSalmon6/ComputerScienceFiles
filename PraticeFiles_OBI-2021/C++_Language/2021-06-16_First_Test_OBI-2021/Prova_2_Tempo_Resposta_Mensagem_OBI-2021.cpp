9acb27fb072354ddd6639d46048a49e39915fe27
Jean Achour <59932224+SeawardSalmon6@users.noreply.github.com>
2021-07-07 15:48:12 -0300

    Updating Answer - "Tempo de Resposta"
    Updating the code for better approach with the Answers of the Test.

--- PraticeFiles_OBI-2021/C++_Language/2021-06-16_First_Test_OBI-2021/Prova_2_Tempo_Resposta_Mensagem_OBI-2021.cpp
+++ PraticeFiles_OBI-2021/C++_Language/2021-06-16_First_Test_OBI-2021/Prova_2_Tempo_Resposta_Mensagem_OBI-2021.cpp
@@ -3,33 +3,31 @@
 using namespace std;
 
 int main() {
-	int numReg;
-	int amigo   = 0,
-		max     = 0;
+	int  numReg;
+	int  amigo  = 0,
+		 max    = 0;
 	char verify = 'S';
 	
 	cin >> numReg;
 	
-	char id[numReg]     = { 0 };
-	int num[numReg]     = { 0 },
-		timer[101]      = { 0 },
-		counterRec[101] = { 0 },
-		counterEnv[101] = { 0 };
+	char id[numReg]      = { 0 };
+	int  num[numReg]     = { 0 },
+		 timer[100000]      = { 0 },
+		 counterRec[100000] = { 0 },
+		 counterEnv[100000] = { 0 };
 	
 	for(int i = 0; i < numReg; i++) {
 		cin >> id[i];
 		cin >> num[i];
 		
-		if(num[i] > max) {
+		if(num[i] > max)
 			max = num[i];
-		}
 
-		if(id[i] == 'R') {
+		if(id[i] == 'R')
 			counterRec[num[i]]++;
 			
-		} else if(id[i] == 'E') {
+		else if(id[i] == 'E')
 			counterEnv[num[i]]++;
-		}
 	}
 	
 	for(int i = 0; i <= max; i++) {
@@ -53,39 +51,41 @@ int main() {
 				amigo = t;
 				break;
 			}
-			else if(t == max) { verify = 'Q'; amigo = t; }
+			else if(t == max)
+				verify = 'Q'; amigo = t;
 		}
 		
 		for(int i = 0; i < numReg; i++)
 		{	
 			if(verify != 'Q')
 			{
-			    if(id[i] == 'E' && num[i] == amigo) {
+			    if(id[i] == 'E' && num[i] == amigo)
 					verify = 'E';
 
-				} else if(id[i] == 'R' && num[i] == amigo && counterRec[amigo] > 0) {
+				else if(id[i] == 'R' && num[i] == amigo && counterRec[amigo] > 0)
 					verify = 'S';
-				}
     			
     			if(verify == 'S')
     			{
-    			    if(id[i] == 'T') {
+    			    if(id[i] == 'T')
     			        timer[amigo] += num[i] - 1;
     			        
-    			    } else if(counterRec[amigo] > 0) {
+    			    else if(counterRec[amigo] > 0)
     			        timer[amigo]++;
-    			    }
     			}
     			
-			} else { break; }
+    			if(amigo == max && i == numReg - 1)
+				    verify = 'Q';
+			}
+			else
+				break;
 		}
 		
-		if(counterRec[amigo] == -1) {
+		if(counterRec[amigo] == -1)
 			cout << amigo << " -1\n";
 		
-		} else if(counterRec[amigo] > 0) {
+		else if(counterRec[amigo] > 0)
 			cout << amigo << " " << timer[amigo] << endl;
-		}
 	}
 	
 	return 0;
