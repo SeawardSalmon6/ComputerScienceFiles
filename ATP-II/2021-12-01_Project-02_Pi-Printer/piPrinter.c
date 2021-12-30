63bfd952466bc4620a30d61800ab72a21d0b815f
SeawardSalmon6 <rayhanachour@hotmail.com>
2021-12-30 19:57:39 -0300

    Pushing last changes

--- ATP-II/2021-12-01_Project-02_Pi-Printer/piPrinter.c
+++ ATP-II/2021-12-01_Project-02_Pi-Printer/piPrinter.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-// protótipos das funções utilizadas/criadas
+// prototipos das funcoes utilizadas/criadas
 double CalcularPI(int);
 double PrimeiroTermo(int);
 double SegundoTermo(int);
@@ -8,7 +8,7 @@ double TerceiroTermo(double);
 int NumeroPCasa(double, int);
 
 void LerCasasDecimais(int *P) {
-	do { // loop para forçar valores entre 1 <= P <= 20
+	do { // loop para forcar valores entre 1 <= P <= 20
 		printf("\nInsira a quantidade de casas decimais: ");
 		scanf(" %d", P);
 	} while(*P < 1 || *P > 15);
@@ -16,24 +16,24 @@ void LerCasasDecimais(int *P) {
 
 int main() {
     int P; // quantidade de casas a serem exibidas
-	double pi = 0; // variável de alocação do valor de PI
+	double pi = 0; // variavel de alocacao do valor de PI
 
 	LerCasasDecimais(&P);
 	pi = CalcularPI(P);
 
-	printf("\n-- O valor de pi é: %.*lf\n\n", P, pi);
+	printf("\n-- PI: %.*lf\n\n", P, pi);
 
 	return 0;
 }
 
-// retorna o número posicionado na posição P em Pi-Parcial
+// retorna o numero posicionado na posicao P em Pi-Parcial
 int NumeroPCasa(double val, int P) {
 	while(P--)
         val = (val * 10) - ((int) val) * 10;
     return val;
 }
 
-// Funções-Termos das Parcelas
+// Funcoes-Termos das Parcelas
 double PrimeiroTermo(int num) {
 	return (num / (num + 1.0));
 }
@@ -44,33 +44,33 @@ double TerceiroTermo(double val) {
 	return (val * 0.25); // como P no Terceiro Termo aumenta em 2 == n^(p+2) == (0.5)^2 == 0.25
 }
 
-// calcula o valor de PI, utilizando a fórmula de Newton
+// calcula o valor de PI, utilizando a formula de Newton
 double CalcularPI(int P) {
-	int cont = 0; // variável para contar as três repetições da parcela, na posição P
-	int numAux = 1, expoente = 3; // variáveis da fórmula de Newton, para o cálculo de PI
-	int casaAnterior = 0, casaAtual = -1; // variáveis de controle para os testes das parcelas repetidas
+	int cont = 0; // variavel para contar as tres repeticoes da parcela, na posicao P
+	int numAux = 1, expoente = 3; // variavel da formula de Newton, para o calculo de PI
+	int casaAnterior = 0, casaAtual = -1; // variavel de controle para os testes das parcelas repetidas
 
-	// variáveis de alocação dos valores parcial do Pri. e Ter. Termos para poupar processamento
+	// variavel de alocacao dos valores parcial do Pri. e Ter. Termos para poupar processamento
 	double priTermo = 1;
 	double terTermo = 0.5;
 	double piParcial = 3;
 
-	// até que o valor de pi deixe de variar na P-ésima casa, por três parcelas, continue a calcular
+	// ate que o valor de pi deixe de variar na P-esima casa, por tres parcelas, continue a calcular
 	while(cont < 3) {
 		priTermo *= PrimeiroTermo(numAux); // calcula o valor do primeiro termo na parcela N
-		terTermo = TerceiroTermo(terTermo); // calcula o valor do terceiro termo na parcela N em relação ao valor anterior
+		terTermo = TerceiroTermo(terTermo); // calcula o valor do terceiro termo na parcela N em relacao ao valor anterior
 		piParcial += 6 * priTermo * SegundoTermo(expoente) * terTermo; // calcula o valor de PI
 
 		// captura o valor da casa P
-		casaAtual = NumeroPCasa(piParcial, P);
+		casaAtual = NumeroPCasa(piParcial, P); // 3.14159 -> 4
 
 		// testa o valor da casa e soma 1 ou reseta o contador
 		cont = (casaAtual == casaAnterior) ? ++cont : 0;
 		casaAnterior = casaAtual; // atualiza o valor da casaAnterior
 
-		numAux += 2; expoente += 2; // atualiza as variáveis da fórmula, para a próxima parcela
+		numAux += 2; expoente += 2; // atualiza as variavel da formula, para a próxima parcela
 	}
 
-	// retorna o valor de PI, de acordo com a fórmula de Newton
+	// retorna o valor de PI, de acordo com a formula de Newton
 	return (piParcial);
 }
--- ATP-II/2021-12-01_Project-02_Pi-Printer/quadPrinter.c
+++ ATP-II/2021-12-01_Project-02_Pi-Printer/quadPrinter.c
@@ -1,23 +1,11 @@
 #include <stdio.h>
-#include <math.h>
 #include <quadmath.h>
 
-// digite um número: 5
-// ler 5 valores
-/*
-	char format[20] = { 0 };
-	scanf(" %d", &quantidade);
-
-	for(int i = 0; i < quantidade; i++)
-		format = format & " %d";
-*/
-
 // protótipos das funções utilizadas/criadas
-int NumeroPCasa(__float128 parcial, int P, int aux);
-__float128 PrimeiroTermo(__float128 numAux);
-__float128 SegundoTermo(int expoente);
-long double TerceiroTermo(int expoente);
-__float128 CalcularPI(int P);
+int NumeroPCasa(__float128, int);
+__float128 PrimeiroTermo(__float128);
+__float128 SegundoTermo(int);
+__float128 CalcularPI(int);
 
 void LerQuantidadeCasas(int *P) {
 	do { // loop para forçar valores entre 1 <= P <= 20
@@ -29,65 +17,59 @@ void LerQuantidadeCasas(int *P) {
 int main()  {
     int P; // quantidade de casas a serem exibidas
 	__float128 pi = 0; // variável de alocação do valor de PI
-	char piString[50] = { 0 };
+	char piString[53] = { 0 };
 
 	// chama a função que calcula o valor de PI, utilizando a fórmula de Newton
 	LerQuantidadeCasas(&P);
 	pi = CalcularPI(P);
 
 	// transforma o valor de um __float128 em string
-	quadmath_snprintf(piString, sizeof(piString), "%.*Qf", P, pi);
+	quadmath_snprintf(piString, sizeof piString, "%.*Qf", P, pi);
 
 	// printa o resultado após os cálculos
-    printf("\n-- O valor de pi é: %s\n\n", piString);
+    printf("\n-- O valor de pi é: %s\n", piString);
 
 	return 0;
 }
 
 // retorna o número posicionado na posição P em Pi-Parcial
-int NumeroPCasa(__float128 parcial, int P, int aux) {
-	if(aux > P) { // enquanto a qtd. de casas não for pelo menos igual a P, retorne -1
+int NumeroPCasa(__float128 parcial, int P) {
 	    while(--P) // decresce o valor de P até que se encontre o número em P
 	        parcial = (parcial * 10) - ((int) parcial * 10); // multiplica o parcial por 10 e subtrai a dezena
 		return parcial;
-	} else return -1;
 }
 
 __float128 PrimeiroTermo(__float128 numAux) {
 	return (__float128) numAux / (numAux + 1);
 }
-
 __float128 SegundoTermo(int expoente) {
 	return (__float128) 1 / expoente;
-}
-
-long double TerceiroTermo(int expoente) {
-	return powl(0.5, expoente);
 }
 
 // calcula o valor de PI, utilizando a fórmula de Newton + a função auxiliar acima
 __float128 CalcularPI(int P) {
 	int counter = 0; // variável para contar as três repetições da parcela, na posição P
 	int numAux = 1, exp = 3; // variáveis da fórmula de Newton, para o cálculo de PI
-	int casaAnterior = 0, casaAtual = 0; // variáveis de controle para os testes das parcelas repetidas
+	int casaAnterior = -1, casaAtual = -1; // variáveis de controle para os testes das parcelas repetidas
 	__float128 priTermo = 1; // primeiro termo das parcelas
-	__float128 piParcial = 0.5; // variável de armazenamento do valor parcial de PI
+  	__float128 terTermo = 0.5; // primeiro termo das parcelas
+	__float128 piParcial = 3; // variável de armazenamento do valor parcial de PI
 
 	// quando o valor de pi, deixar de variar na P-ésima casa, por três parcelas, continue a calcular
 	while(counter < 3) {
 		priTermo *= PrimeiroTermo(numAux); // calcula o valor do primeiro termo na parcela N
-		piParcial += priTermo * SegundoTermo(exp) * TerceiroTermo(exp); // calcula o valor de PI / 6
+    	terTermo *= 0.25;
+		piParcial += 6 * priTermo * SegundoTermo(exp) * terTermo; // calcula o valor de PI / 6
 
 		// captura o valor da casa P
-		casaAtual = NumeroPCasa(piParcial * 6, P, numAux);
+		casaAtual = NumeroPCasa(piParcial, P);
 		counter = (casaAtual == casaAnterior) ? ++counter : 0; // testa o valor da casa e soma 1 ou reseta o contador
 
 		// se o retorno de NumeroPCasa for nulo, a casaAnterior não se altera
-		if(casaAtual >= 0)
-			casaAnterior = casaAtual;
+		casaAnterior = casaAtual;
 
 		numAux += 2; exp += 2; // atualiza as variáveis da fórmula, para a próxima parcela
 	}
 
-	return piParcial * 6; // retorna o valor de PI, de acordo com a fórmula de Newton
+	return piParcial; // retorna o valor de PI, de acordo com a fórmula de Newton
 }
