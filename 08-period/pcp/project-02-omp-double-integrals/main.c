d052b79fdce68e34dfbef1601bac277c17f55854
SeawardSalmon6 <rayhanachour@hotmail.com>
2024-11-01 17:34:08 -0300

    feat(project-02): add updates

--- 08-period/pcp/project-02-omp-double-integrals/main.c
+++ 08-period/pcp/project-02-omp-double-integrals/main.c
@@ -3,17 +3,17 @@
 #include <math.h>
 #include <omp.h>
 
-const double LOWER_BOUNDS[2] = { 0, 0 };
-const double UPPER_BOUNDS[2] = { 1.5, 1.5 };
+const double LOWER_BOUNDS[] = { 0, 0 };
+const double UPPER_BOUNDS[] = { 1.5, 1.5 };
 
-const int NUM_OF_TESTS_RUNS = 10;
-const int POINTS_LENGTH = 1;
-const int THREADS_LENGTH = 4;
-const int THREADS_TESTS[THREADS_LENGTH] = { 1, 2, 4, 8 };
-const long POINTS_TESTS[POINTS_LENGTH] = {
-  (long)10e3,
-  (long)10e4,
-  (long)10e5
+const int NUM_OF_TESTS_RUNS = 1;
+const int INTERVALS_LENGTH = 3;
+const int THREADS_LENGTH = 1;
+const int THREADS_TESTS[] = { 8 };
+const long INTERVALS_TESTS[] = {
+  (long)1e4, // 10^3
+  (long)1e5, // 10^4
+  (long)1e6  // 10^5
 };
 
 double f(const double x, const double y) {
@@ -21,9 +21,14 @@ double f(const double x, const double y) {
   return sin(x * x + y * y);
 }
 
-double double_trapz(long points[POINTS_LENGTH]) {
-  const double HX = (UPPER_BOUNDS[0] - LOWER_BOUNDS[0]) / (points[0] - 1);
-  const double HY = (UPPER_BOUNDS[1] - LOWER_BOUNDS[1]) / (points[1] - 1);
+double double_trapz(const long points[2]) {
+  const long X_INTERVALS = points[0] - 1;
+  const long Y_INTERVALS = points[1] - 1;
+
+  const double HX = (UPPER_BOUNDS[0] - LOWER_BOUNDS[0]) / X_INTERVALS;
+  const double HX_HALF = HX / 2.0;
+  const double HY = (UPPER_BOUNDS[1] - LOWER_BOUNDS[1]) / Y_INTERVALS;
+  const double HY_HALF = HY / 2.0;
 
   double global_result = 0.0;
 
@@ -35,59 +40,70 @@ double double_trapz(long points[POINTS_LENGTH]) {
     long outer_start = points[0] * thread_id / num_threads;
     long outer_end = points[0] * (thread_id + 1) / num_threads;
 
+    double x, inner_result;
     double local_result = 0.0;
 
     for (long i = outer_start; i < outer_end; i++) {
-      double x = LOWER_BOUNDS[0] + i * HX;
-      double inner_result = 0.0;
+      x = LOWER_BOUNDS[0] + i * HX;
+      inner_result = 0.0;
 
       for (long j = 0; j < points[1]; j++) {
         double y = LOWER_BOUNDS[1] + j * HY;
         double fxy = f(x, y);
-        inner_result += (j == 0 || j == points[1] - 1) ? fxy : 2 * fxy;
+        inner_result += (j == 0 || j == Y_INTERVALS) ? fxy : 2 * fxy;
       }
 
-      inner_result *= HY / 2.0;
-      local_result += (i == 0 || i == points[0] - 1) ? inner_result : 2 * inner_result;
+      inner_result *= HY_HALF;
+      local_result += (i == 0 || i == X_INTERVALS) ? inner_result : 2 * inner_result;
     }
 
-    global_result += local_result * HX / 2.0;
+    global_result += local_result * HX_HALF;
   }
 
   return global_result;
 }
 
 int main() {
-  int num_of_threads = 0;
+  int i, j, k, t, num_of_threads = 0;
   long points[2] = { 0, 0 };
-  double start, end;
+  double start, end, suit_time = 0.0, total_time = 0.0;
 
-  for (int t = 0; t < NUM_OF_TESTS_RUNS; t++) {
-    for (int k = 0; k < THREADS_LENGTH; k++) {
+  for (t = 0; t < NUM_OF_TESTS_RUNS; t++) {
+    printf("---- Test %d results ----\n", (t + 1));
+    suit_time = 0.0;
+
+    for (k = 0; k < THREADS_LENGTH; k++) {
       num_of_threads = THREADS_TESTS[k];
       omp_set_num_threads(num_of_threads);
 
-      for (int i = 0; i < POINTS_LENGTH; i++) {
-        for (int j = 0; j < POINTS_LENGTH; j++) {
+      printf("    ---- %d Threads results ----\n", num_of_threads);
 
-          points[0] = POINTS_TESTS[i] + 1;
-          points[1] = POINTS_TESTS[j] + 1;
+      for (i = 0; i < INTERVALS_LENGTH; i++) {
+        for (j = 0; j < INTERVALS_LENGTH; j++) {
+          points[0] = INTERVALS_TESTS[i] + 1;
+          points[1] = INTERVALS_TESTS[j] + 1;
 
           start = omp_get_wtime();
           double result = double_trapz(points);
           end = omp_get_wtime();
 
-          printf("---- Test %d: Threads (%d), Points (%d, %d) results ----\n", (t + 1), (k + 1), (i + 1), (j + 1));
-          printf("- Result: %.8lf\n", result);
-          printf("- Time elapsed: %.8lf seconds with\n", end - start);
-          printf("  => Number of threads: %d\n", num_of_threads);
-          printf("  => X intervals: %ld\n", points[0] - 1);
-          printf("  => Y intervals: %ld\n", points[1] - 1);
+          double time = end - start;
+          suit_time += time;
+          total_time += time;
+
+          printf("        ---- Intervals (%ld, %ld) results ----\n", points[0] - 1, points[1] - 1);
+          printf("            - Result: %.16lf\n", result);
+          printf("            - Time elapsed: %.16lf seconds\n", time);
           printf("\n\n");
         }
       }
     }
+
+    printf("==== Test %d total time elapsed: %.16lf seconds\n\n", (t + 1), suit_time);
   }
+
+  printf("\n---- Total suits time elapsed: %.16lf seconds", total_time);
+  printf("\n    - Average time elapsed: %.16lf seconds\n", total_time / NUM_OF_TESTS_RUNS);
 
   return 0;
 }
