cc3c80ea45008d148edb885bfb9006a2a4e56c38
SeawardSalmon6 <rayhanachour@hotmail.com>
2024-11-28 16:46:44 -0300

    feat(pcp): add project-02 with mpi

--- 08-period/pcp/project-03-mpi-double-integrals/main.c
+++ 08-period/pcp/project-03-mpi-double-integrals/main.c
@@ -1,116 +1,125 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-#include <omp.h>
+#include <mpi.h>
+
+#define LOGGER_FILENAME "log.csv"
 
 #define LOWER_BOUND 0
 #define UPPER_BOUND 1.5
 
-#define NUM_OF_TESTS_RUNS 2
-#define INTERVALS_LENGTH 3
-#define THREADS_LENGTH 4
+#define ROOT_PROCESS 0
 
-const int THREADS_TESTS[] = { 8, 4, 2, 1 };
-const long INTERVALS_TESTS[] = {
-  (long)1e4, // 10^3
-  (long)1e5, // 10^4
-  (long)1e6  // 10^5
-};
+#define TEST_NUM 1
+const long X_POINTS = (long)1e4; // 10^3
+const long Y_POINTS = (long)1e4; // 10^3
+
+// #define TEST_NUM 2
+// const long X_POINTS = (long)1e4; // 10^3
+// const long Y_POINTS = (long)1e5; // 10^4
+
+// #define TEST_NUM 3
+// const long X_POINTS = (long)1e4; // 10^3
+// const long Y_POINTS = (long)1e6; // 10^5
+
+/* ==================================== */
+
+// #define TEST_NUM 4
+// const long X_POINTS = (long)1e5; // 10^4
+// const long Y_POINTS = (long)1e4; // 10^3
+
+// #define TEST_NUM 5
+// const long X_POINTS = (long)1e5; // 10^4
+// const long Y_POINTS = (long)1e5; // 10^4
+
+// #define TEST_NUM 6
+// const long X_POINTS = (long)1e5; // 10^4
+// const long Y_POINTS = (long)1e6; // 10^5
+
+/* ==================================== */
+
+// #define TEST_NUM 7
+// const long X_POINTS = (long)1e6; // 10^5
+// const long Y_POINTS = (long)1e4; // 10^3
+
+// #define TEST_NUM 8
+// const long X_POINTS = (long)1e6; // 10^5
+// const long Y_POINTS = (long)1e5; // 10^4
+
+// #define TEST_NUM 9
+// const long X_POINTS = (long)1e6; // 10^5
+// const long Y_POINTS = (long)1e6; // 10^5
 
 double f(const double x, const double y) {
   // f(x, y) = sin(x² + y²)
   return sin(x * x + y * y);
 }
 
-double double_trapz(const long points[2]) {
-  const long X_INTERVALS = points[0] - 1;
-  const long Y_INTERVALS = points[1] - 1;
+double double_trapz(const int my_rank, const int world_size) {
+  const long X_INTERVALS = X_POINTS - 1;
+  const long Y_INTERVALS = Y_POINTS - 1;
 
   const double HX = (UPPER_BOUND - LOWER_BOUND) / X_INTERVALS;
   const double HX_HALF = HX / 2.0;
   const double HY = (UPPER_BOUND - LOWER_BOUND) / Y_INTERVALS;
   const double HY_HALF = HY / 2.0;
 
-  double global_result = 0.0;
+  long outer_start = X_POINTS * my_rank / world_size;
+  long outer_end = X_POINTS * (my_rank + 1) / world_size;
 
-  #pragma omp parallel reduction(+:global_result)
-  {
-    int num_threads = omp_get_num_threads();
-    int thread_id = omp_get_thread_num();
+  double x, inner_result, fxy;
+  double local_result = 0.0;
 
-    long outer_start = points[0] * thread_id / num_threads;
-    long outer_end = points[0] * (thread_id + 1) / num_threads;
+  for (long i = outer_start; i < outer_end; i++) {
+    x = LOWER_BOUND + i * HX;
+    inner_result = 0.0;
 
-    double x, inner_result, fxy;
-    double local_result = 0.0;
-
-    for (long i = outer_start; i < outer_end; i++) {
-      x = LOWER_BOUND + i * HX;
-      inner_result = 0.0;
-
-      for (long j = 0; j < points[1]; j++) {
-        fxy = f(x, LOWER_BOUND + j * HY);
-        inner_result += (j == 0 || j == Y_INTERVALS) ? fxy : 2 * fxy;
-      }
-
-      inner_result *= HY_HALF;
-      local_result += (i == 0 || i == X_INTERVALS) ? inner_result : 2 * inner_result;
+    for (long j = 0; j < Y_POINTS; j++) {
+      fxy = f(x, LOWER_BOUND + j * HY);
+      inner_result += (j == 0 || j == Y_INTERVALS) ? fxy : 2 * fxy;
     }
 
-    #pragma omp atomic
-    global_result += local_result * HX_HALF;
+    inner_result *= HY_HALF;
+    local_result += (i == 0 || i == X_INTERVALS) ? inner_result : 2 * inner_result;
   }
 
+  local_result *= HX_HALF;
+
+  double global_result = 0.0;
+  MPI_Reduce(&local_result, &global_result, 1, MPI_DOUBLE, MPI_SUM, ROOT_PROCESS, MPI_COMM_WORLD);
   return global_result;
 }
 
-int main() {
-  int i, j, k, t, num_of_threads = 0;
-  long points[2] = { 0, 0 };
-  double start, end, suit_time = 0.0, total_time = 0.0;
+int main(int argc, char *argv[]) {
+  int my_rank, world_size;
+  double end, start;
 
-  for (t = 0; t < NUM_OF_TESTS_RUNS; t++) {
-    printf("---- Test %d results ----\n", (t + 1));
-    suit_time = 0.0;
+  MPI_Init(&argc, &argv);
+  MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+  MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
 
-    for (k = 0; k < THREADS_LENGTH; k++) {
-      num_of_threads = THREADS_TESTS[k];
-      omp_set_num_threads(num_of_threads);
+  MPI_Barrier(MPI_COMM_WORLD);
+  start = MPI_Wtime();
 
-      printf("    ---- %d Threads results ----\n", num_of_threads);
+  double result = double_trapz(my_rank, world_size);
 
-      /*
-        For reproductibility of the tests in the report,
-          uncomment the line below:
-          
-        for (i = 0; i < 1; i++) { // Refers only to first interval of the outer integral
-       */
-      for (i = 0; i < INTERVALS_LENGTH; i++) {
-        for (j = 0; j < INTERVALS_LENGTH; j++) {
-          points[0] = INTERVALS_TESTS[i] + 1;
-          points[1] = INTERVALS_TESTS[j] + 1;
+  MPI_Barrier(MPI_COMM_WORLD);
+  end = MPI_Wtime();
 
-          start = omp_get_wtime();
-          double result = double_trapz(points);
-          end = omp_get_wtime();
+  MPI_Finalize();
 
-          double time = end - start;
-          suit_time += time;
-          total_time += time;
+  if (my_rank == ROOT_PROCESS) {
+    FILE *logger_fp = fopen(LOGGER_FILENAME, "a+");
 
-          printf("        ---- Intervals (%ld, %ld) results ----\n", points[0] - 1, points[1] - 1);
-          printf("            - Result: %.16lf\n", result);
-          printf("            - Time elapsed: %.16lf seconds\n", time);
-          printf("\n\n");
-        }
-      }
-    }
+    fprintf(logger_fp, "\"%d\";", TEST_NUM);          // Test ID
+    fprintf(logger_fp, "\"%d\";", world_size);        // Processes length
+    fprintf(logger_fp, "\"%ld\";", X_POINTS);         // X points
+    fprintf(logger_fp, "\"%ld\";", Y_POINTS);         // Y points
+    fprintf(logger_fp, "\"%.16lf\";", result);        // Result
+    fprintf(logger_fp, "\"%.16lf\";\n", end - start); // Time elapsed
 
-    printf("==== Test %d total time elapsed: %.16lf seconds\n\n", (t + 1), suit_time);
+    fclose(logger_fp);
   }
-
-  printf("\n---- Total suits time elapsed: %.16lf seconds", total_time);
-  printf("\n    - Average time elapsed: %.16lf seconds\n", total_time / NUM_OF_TESTS_RUNS);
 
   return 0;
 }
